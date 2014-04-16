5b0666de1ca7b75a5e233f715b09606709d6f258
Andrew Kanieski <andrew.legacy@gmail.com>
2014-04-16 18:16:55 -0400

    Added heroku error handling

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 /**
  * Created by andrew on 4/14/14.
  */
+try {
+    var express = require('express');
+    var morgan = require('morgan');
+    var bodyParser = require('body-parser');
+    var methodOverride = require('method-override');
+    var cookieParser = require('cookie-parser');
+    var session = require('express-session');
+    var app = express();
+    var utils = require('./utils.js');
 
-var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var app = express();
-var utils = require('./utils.js');
-
-app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); 					        // log every request to the console
-app.use(bodyParser()); 						        // pull information from html in POST
-app.use(methodOverride()); 					        // simulate DELETE and PUT
-app.use(cookieParser())                             // Used to maintain sessions
-app.use(session({                                   // Session initialization settings
-    secret: 'telapi demo project',
-    cookie: { maxAge: 60000 }}));
+    app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
+    app.use(morgan('dev')); 					        // log every request to the console
+    app.use(bodyParser()); 						        // pull information from html in POST
+    app.use(methodOverride()); 					        // simulate DELETE and PUT
+    app.use(cookieParser())                             // Used to maintain sessions
+    app.use(session({                                   // Session initialization settings
+        secret: 'telapi demo project',
+        cookie: { maxAge: 60000 }}));
 // Install Controllers
-require('./controllers/telapi.js').install(app);
+    require('./controllers/telapi.js').install(app);
 
-app.listen(80 | process.env.PORT);
-console.log('TelAPI Group Messager started on port ' + (80 | process.env.PORT)); 			// shoutout to the user
+    app.listen(80 | process.env.PORT);
+    console.log('TelAPI Group Messager started on port ' + (80 | process.env.PORT)); 			// shoutout to the user
+} catch (err) {
+    console.log(err);
+    throw err;
+}
